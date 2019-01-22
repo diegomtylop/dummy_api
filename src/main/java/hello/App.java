558e67534336d1f46a581a445af49ee695a74b30
d.montoya <d.montoya@192.168.2.1>
2019-01-22 13:30:29 -0500

    App changed

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -12,7 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "[Dummy-api] Initializing Spring boot application!" );
         SpringApplication.run( App.class, args );
     }
 }
